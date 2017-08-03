f6984b6ee483f9ee81574012a235e1a260dd0c65
Elliot Snow-Kropla <esnowkropla@gmail.com>
2017-08-03 15:46:48 -0400

    Add an admin page

---
+++ src/flasher/mod.rs
@@ -0,0 +1,38 @@
+use serde::ser::{Serialize, Serializer, SerializeStruct};
+
+use rocket::request::FlashMessage;
+use std::ops::Deref;
+use UserContext;
+
+pub struct Flasher(pub FlashMessage);
+
+impl Deref for Flasher {
+    type Target = FlashMessage;
+    fn deref(&self) -> &FlashMessage {
+        &self.0
+    }
+}
+
+impl Serialize for Flasher {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("FlashMessage", 2)?;
+        state.serialize_field("name", self.name())?;
+        state.serialize_field("msg", self.msg())?;
+        state.end()
+    }
+}
+
+impl<'a> Serialize for UserContext<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("UserContext", 2)?;
+        state.serialize_field("name", &self.name)?;
+        state.serialize_field("flash", &self.flash)?;
+        state.end()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,34 @@ extern crate rocket_contrib;
 
 #[macro_use]
 extern crate serde_derive;
+extern crate serde;
 
 use rocket::http::{Cookie, Cookies};
-use rocket::response::Redirect;
-use rocket::request::Form;
+use rocket::response::{Redirect, Flash};
+use rocket::request::{self, Form, FlashMessage, FromRequest, Request};
+use rocket::Outcome;
 
 use rocket_contrib::Template;
 
 pub mod useragent;
+pub mod flasher;
 
-#[derive(Serialize)]
 struct UserContext<'a> {
-    name: &'a str,
+    pub name: Option<&'a str>,
+    flash: Option<flasher::Flasher>,
 }
 
-#[derive(Serialize)]
-struct Blank;
+struct Admin;
+
+impl<'a, 'r> FromRequest<'a, 'r> for Admin {
+    type Error = ();
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Admin, ()> {
+        match request.cookies().get_private("name") {
+            Some(ref cookie) if cookie.value() == "Elliot" => Outcome::Success(Admin),
+            _ => Outcome::Forward(()),
+        }
+    }
+}
 
 #[derive(FromForm)]
 struct Name {
@@ -35,31 +47,65 @@ fn name(user_form: Form<Name>, mut cookies: Cookies) -> Redirect {
 }
 
 #[get("/logout")]
-fn logout(mut cookies: Cookies) -> Redirect {
+fn logout(mut cookies: Cookies) -> Flash<Redirect> {
     cookies.remove_private(Cookie::named("name"));
-    Redirect::to("/")
+    Flash::success(Redirect::to("/"), "Successfully logged out")
+}
+
+#[get("/admin", rank = 1)]
+fn admin_panel(admin: Admin, flash: Option<FlashMessage>) -> Template {
+    let flasher = match flash {
+        Some(msg) => Some(flasher::Flasher(msg)),
+        None => None,
+    };
+    let context = UserContext {
+        name: None,
+        flash: flasher,
+    };
+    Template::render("admin", &context)
+}
+
+#[get("/admin", rank = 2)]
+fn admin_panel_user() -> Flash<Redirect> {
+    Flash::error(
+        Redirect::to("/"),
+        "Sorry, you're not allowed on the admin page",
+    )
 }
 
 #[get("/")]
-fn index(mut cookies: Cookies) -> Template {
-    let blank = Blank;
+fn index(flash: Option<FlashMessage>, mut cookies: Cookies) -> Template {
+    let flasher = match flash {
+        Some(msg) => Some(flasher::Flasher(msg)),
+        None => None,
+    };
     match cookies.get_private("name") {
         Some(cookie) => {
-            let context = UserContext { name: cookie.value() };
-            return Template::render("user", &context);
+            return Template::render(
+                "user",
+                &UserContext {
+                    name: Some(cookie.value()),
+                    flash: flasher,
+                },
+            );
         }
-        None => return Template::render("index", &blank),
+        None => {
+            return Template::render(
+                "index",
+                &UserContext {
+                    name: None,
+                    flash: flasher,
+                },
+            )
+        }
     };
-}
-
-#[get("/user/<name>")]
-fn user(name: String) -> Template {
-    let context = UserContext { name: &name };
-    Template::render("user", context)
 }
 
 pub fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/", routes![index, user, name, logout])
+        .mount(
+            "/",
+            routes![index, name, logout, admin_panel, admin_panel_user],
+        )
         .attach(Template::fairing())
 }
